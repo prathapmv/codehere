b8ffd7b339754f8a05fc6e587dbadc0d17a804f2
Prathap <65905793+prathapmv@users.noreply.github.com>
2022-03-10 13:48:27 +0530

    spring practice project

---
+++ springboot/springsecuritydemo/src/test/java/com/prathapmv/springsecuritydemo/SpringsecuritydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prathapmv.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecuritydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
